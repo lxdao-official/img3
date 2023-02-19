2356f35a5decdc3ee78208383b08149d36ee11c9
noyobo <noyobo@gmail.com>
2023-02-19 12:55:23 +0800

    docs: homepage up

--- apps/docs/components/Features/Featrue.tsx
+++ apps/docs/components/Features/Featrue.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  background-color: transparent;
+  flex: 1;
+  & + & {
+    margin-left: 20px;
+  }
+  @media (max-width: 650px) {
+    margin-top: 50px;
+    margin-left: 0 !important;
+  }
+`;
+
 const IconWrapper = styled.div`
   width: 106px;
   height: 106px;
@@ -10,8 +22,8 @@ const IconWrapper = styled.div`
   justify-content: center;
   align-items: center;
   @media (max-width: 650px) {
-    width: 50px;
-    height: 50px;
+    width: 60px;
+    height: 60px;
     float: left;
     margin-right: 20px;
   }
@@ -20,7 +32,7 @@ const IconWrapper = styled.div`
 const Title = styled.h3`
   height: 38px;
   font-weight: 700;
-  font-size: min(28px, max(16px, calc(var(--rpx) * 4)));
+  font-size: 2.25rem;
   line-height: 34px;
   color: #101828;
   padding-top: calc(var(--rpx) * 4);
@@ -28,7 +40,7 @@ const Title = styled.h3`
   @media (max-width: 650px) {
     padding-top: 0;
     padding-bottom: 0;
-    line-height: 50px;
+    line-height: 60px;
   }
 `;
 
@@ -40,14 +52,23 @@ const IconItem = styled.div`
 
 const Description = styled.div`
   font-weight: 400;
-  font-size: 16px;
+  font-size: 1.15625rem;
   line-height: 1.675;
   color: #666f85;
   margin-top: 16px;
   clear: both;
   @media (max-width: 650px) {
-    font-size: 14px;
-    letter-spacing: -0.1px;
+    //  font-size: 14px;
+    //  letter-spacing: -0.1px;
+    padding-top: 20px;
+  }
+`;
+
+export const FeaturesWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 650px) {
+    flex-direction: column;
   }
 `;
 
@@ -59,12 +80,12 @@ export const Feature: React.FC<
   }>
 > = (props) => {
   return (
-    <div>
+    <Wrapper>
       <IconWrapper>
         <IconItem>{props.icon}</IconItem>
       </IconWrapper>
       <Title>{props.title}</Title>
       <Description>{props.description}</Description>
-    </div>
+    </Wrapper>
   );
 };
--- apps/docs/components/Features/index.tsx
+++ apps/docs/components/Features/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Col, Container, Grid, Spacer } from '@nextui-org/react';
-import { Feature } from '@/components/Features/Featrue';
+import { Feature, FeaturesWrapper } from '@/components/Features/Featrue';
 import styled from 'styled-components';
 import { Icon } from '@iconify/react';
 
 const SubTitle = styled.div`
   font-weight: 500;
-  font-size: 21px;
+  font-size: max(14px, min(21px, calc(var(--rpx) * 2)));
   line-height: 60px;
   letter-spacing: -0.02em;
   text-transform: capitalize;
@@ -15,7 +15,8 @@ const SubTitle = styled.div`
 
 const Quote = styled.p`
   font-weight: 500;
-  font-size: max(16px, calc(var(--rpx) * 3));
+  //font-size: min(1.6rem, max(16px, calc(var(--rpx) * 3)));
+  font-size: 1.6rem;
   line-height: 1.5;
   letter-spacing: -0.02em;
   color: #101828;
@@ -40,48 +41,42 @@ export const Features: React.FC<React.PropsWithChildren<any>> = (props) => {
           It provides the easiest way for you to implement images rendering, uploading, etc. based on Web3 storage like
           IPFS.
         </Quote>
-        <Grid.Container gap={1}>
-          <Grid md={4} sm={4} xs={12}>
-            <Feature
-              icon={<Icon fontSize={36} color={'#31E2C7'} icon="material-symbols:image-rounded" />}
-              title={'Img3'}
-              description={
-                '<Img3 /> is a fundamental components for Web3 Apps. It extends HTML <img /> with Web3 decentralization storage, like IPFS. With <Img3 /> you can put ipfs:// in the src and render the image from IPFS with the fastest gateway. Will support ar soon.'
-              }
-            />
-          </Grid>
-          <Grid md={4} sm={4} xs={12}>
-            <Feature
-              icon={<Icon fontSize={36} color={'#31E2C7'} icon="material-symbols:cloud-upload" />}
-              title={'Uploader3'}
-              description={
-                'Uploader3 is a React-based Web3 image upload component that supports multiple image uploads, image cropping, and uploading images to Web3 Storage providers (like IPFS). There are two ways for uploading, by using a backend API or the Uploader3 Connector.'
-              }
-            />
-          </Grid>
-          <Grid md={4} sm={4} xs={12}>
-            <Feature
-              icon={
-                <span
-                  style={{
-                    background: '#31E2C7',
-                    borderRadius: 6,
-                    color: 'white',
-                    fontWeight: 700,
-                    fontSize: 12,
-                    padding: '3px 6px',
-                  }}
-                >
-                  SDK
-                </span>
-              }
-              title={'SDK connector'}
-              description={
-                'Uploader3 Connector is a connector for Uploader3. Currently, it only supports NFT.storage IPFS service provider.'
-              }
-            />
-          </Grid>
-        </Grid.Container>
+        <FeaturesWrapper>
+          <Feature
+            icon={<Icon fontSize={36} color={'#31E2C7'} icon="material-symbols:image-rounded" />}
+            title={'Img3'}
+            description={
+              '<Img3 /> is a fundamental components for Web3 Apps. It extends HTML <img /> with Web3 decentralization storage, like IPFS. With <Img3 /> you can put ipfs:// in the src and render the image from IPFS with the fastest gateway. Will support ar soon.'
+            }
+          />
+          <Feature
+            icon={<Icon fontSize={36} color={'#31E2C7'} icon="material-symbols:cloud-upload" />}
+            title={'Uploader3'}
+            description={
+              'Uploader3 is a React-based Web3 image upload component that supports multiple image uploads, image cropping, and uploading images to Web3 Storage providers (like IPFS). There are two ways for uploading, by using a backend API or the Uploader3 Connector.'
+            }
+          />
+          <Feature
+            icon={
+              <span
+                style={{
+                  background: '#31E2C7',
+                  borderRadius: 6,
+                  color: 'white',
+                  fontWeight: 700,
+                  fontSize: 12,
+                  padding: '3px 6px',
+                }}
+              >
+                SDK
+              </span>
+            }
+            title={'SDK connector'}
+            description={
+              'Uploader3 Connector is a connector for Uploader3. Currently, it only supports NFT.storage IPFS service provider.'
+            }
+          />
+        </FeaturesWrapper>
       </Container>
     </Wrapper>
   );
